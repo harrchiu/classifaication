83a6ddf05e2494904bad5a84c95faea36a4f80a8
HC mbp21 <harrchiu@gmail.com>
2023-08-16 01:15:19 -0400

    i think... and i wonder... accurate one third of the time..

--- digit_nn.py
+++ digit_nn.py
@@ -2,6 +2,7 @@
 # https://medium.com/p/68998a08e4f6
 
 import numpy as np
+import copy 
 import csv
 import matplotlib.pyplot as plt
 
@@ -32,8 +33,8 @@ def render_arr(arr):
 class NeuralNetwork:
     def __init__(self, inputN, hiddenN, outputN, loadData):
         # layers/nodes
-        self.hiddenL = None
-        self.outputL = np.zeros(outputN)
+        # self.hiddenL = None
+        # self.outputL = np.zeros(outputN)
 
         # weights/edges
         # inputL to hiddenL; hiddenL to outputL
@@ -48,38 +49,20 @@ class NeuralNetwork:
     
     # pass through input of X
     def feed_fwd(self, X):
-        # self.hiddenL = sigmoid(np.dot(X, self.w1))
-        # self.output_res = softmax(sigmoid(np.dot(self.hiddenL, self.w2)))
-        # # self.output_res = sigmoid(np.dot(self.hiddenL, self.w2))
-        # return self.output_res
-
-        self.x_l1 = np.dot(X, self.w1)
+        self.x_l1 = X.dot(self.w1)
         self.x_sigmoid = sigmoid(self.x_l1)
         self.x_l2 = self.x_sigmoid.dot(self.w2)
         # self.out = softmax(self.x_l2)
 
-        self.out = np.copy(self.x_l2)
-        for ind,x in enumerate(self.out):
+        self.out = copy.deepcopy(self.x_l2)
+        for ind,x in enumerate(self.x_l2):
             self.out[ind] = softmax(x)
         # print(self.sigmoid_out, self.output_res)
         return self.out
     
     # use results Y to adjust weights
     def back_propogate(self, X, Y):
-        # loss_calc = 2 * (Y - self.outputL) * sigmoid_drtv(self.outputL)
-        # loss_calc = 2 * (Y - self.outputL)/10 * d_softmax(self.sigmoid_out)
-        # w2_delta = np.dot(self.hiddenL.T, loss_calc)
-        # w1_delta = np.dot(
-        #     X.T, np.dot(loss_calc, self.w2.T) * sigmoid_drtv(self.hiddenL)
-        # )
-
-        # loss_calc = 2 * (Y - self.outputL)/10 * d_softmax(self.sigmoid_out)
-        # w2_delta = np.dot(self.hiddenL.T, loss_calc)
-        # w1_delta = np.dot(
-        #     X.T, np.dot(self.w2, loss_calc.T).T * sigmoid_drtv(self.x1_p)
-        # )
-
-        error=2*(Y - self.outputL)/self.out.shape[0]*d_softmax(self.x_l2)
+        error=2*(self.out - Y)/self.out.shape[0]*d_softmax(self.x_l2)
         w2_delta=self.x_sigmoid.T@error
         
         
@@ -90,9 +73,12 @@ class NeuralNetwork:
 
         self.w2 -= w2_delta*0.001
         self.w1 -= w1_delta*0.001
+        # print('training', X, Y, self.out)
+        # print(w2_delta, w1_delta)
 
+    # forward and backward pass
     def train(self, X, Y):
-        self.outputL = self.feed_fwd(X)
+        self.out = self.feed_fwd(X)
         self.back_propogate(X, Y)
 
     def saveWeights(self):
@@ -100,30 +86,33 @@ class NeuralNetwork:
         np.save('w2.npy', self.w2)
 
 TRAINING_DATA_LEN = 60000
-# csvfile = open('mnist_train.csv')
-# batch_size = 256
-# inputN = 28*28
-# hiddenN = 200
-# outputN = 10
-csvfile = open('dummy_train.csv')
-TRAINING_DATA_LEN = 1
-batch_size = 8
-inputN = 3
+csvfile = open('mnist_train.csv')
+batch_size = 256
+inputN = 28*28
 hiddenN = 128
-outputN = 2
+outputN = 10
+# csvfile = open('dummy_train.csv')
+# TRAINING_DATA_LEN = 9
+# batch_size = 3
+# inputN = 12
+# hiddenN = 128
+# outputN = 4
 data = np.loadtxt(csvfile, delimiter=',')
-loadData = False # set false to start training from scratch
+loadData = True # set false to start training from scratch
 
 # load set
 with open('sample_num.txt','r') as file:
-    sample_num = int(file.read())
+    try:
+        sample_num = int(file.read())
+    except:
+        sample_num = 0
     if loadData == False or sample_num + batch_size >= TRAINING_DATA_LEN:
         sample_num = 0
 
 nn = NeuralNetwork(inputN, hiddenN, outputN, loadData)
 
-accuracy = 100
-for epoch in range(20000):
+accuracies = []
+for epoch in range(2000):
 
     # get current slice of training data and call function
     while sample_num < TRAINING_DATA_LEN:
@@ -139,30 +128,42 @@ for epoch in range(20000):
             Y[ind][int(ans)] = 1.0
             # Y[ind][0] = outputs[ind]
         nn.train(X, Y)
+        correct = 0
+        for ind,res in enumerate(nn.out):
+            if np.argmax(res) == int(outputs[ind]):
+                correct += 1
+        accuracies.append(correct*1.0/batch_size)
+
         sample_num += batch_size
     sample_num = 0
 
-    if epoch % 1000 == 0:
+    if epoch % 10 == 0:
+        print(accuracies[-1])
         print("--- epoch #", epoch)
-        # print("Input : \n" + str(X))
-        print("Actual Output: \n" + str(Y))
-        print("Predicted Output: \n" + str(nn.feed_fwd(X)))
+        # print("Input : \n" + str(X[0]))
+        print("Actual Output: \n" + str(Y[0]))
+        print("Predicted Output: \n" + str(nn.feed_fwd(X)[0]))
         print(
             "Loss: " + str(np.mean(np.square(Y - nn.feed_fwd(X))))
         )  # mean sum squared loss
 
-    if (epoch % 10 == 0):
+    if (epoch % 50 == 0):
         nn.saveWeights()
 
 # test new values
-if False and q % 10 == 0:
+if True:
     next_ind = np.random.randint(TRAINING_DATA_LEN)
-    sample_input = data[next_ind][1:]
-    sample_output = data[next_ind][0]
+    sample_input = np.array([data[next_ind][1:]])
+    
+    activated_index_output = int(data[next_ind][0])
+    sample_output = np.zeros(outputN)
+    sample_output[activated_index_output] = 2.0
+
+    print(sample_input, sample_output)
 
     print("\n\n---- Final Testing ---- ")
     # print("Input : \n" + str(X))
     print("Actual Output: \n" + str(sample_output))
     print("Predicted Output: \n" + str(nn.feed_fwd(sample_input)))
-    print("Loss: " + str(np.mean(np.square([[sample_output]] - nn.feed_fwd(sample_input)))))
-    render_arr(sample_input)
+    print("Loss: " + str(np.mean(np.square(sample_output - nn.feed_fwd(sample_input)))))
+    # render_arr(sample_input)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+en('dummy_train.csv')
+TRAINING_DATA_LEN = 8
+batch_size = 8
+inputN = 3
+hiddenN = 128
+outputN = 4
